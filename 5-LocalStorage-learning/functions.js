b050a4fbc294bc72acbb3bc2d65beca4c6913c7b
kubex11-coder <80459898+kubex11-coder@users.noreply.github.com>
2022-10-11 16:23:07 +0200

    dev

--- 5-LocalStorage-learning/functions.js
+++ 5-LocalStorage-learning/functions.js
@@ -16,7 +16,7 @@ const saveNames = function (oneName) {
   localStorage.setItem("names", JSON.stringify(oneName))
 }
 
-// Vypsání dat z local storage
+// Vypsání dat z local storage na stránku + vymazání
 
 const textFieldCreate = function (oneName) {
   const newDiv = document.createElement("div")
@@ -26,9 +26,38 @@ const textFieldCreate = function (oneName) {
   newButton.textContent = "Vymazat položku"
   newDiv.appendChild(newButton)
 
+  newButton.addEventListener("click", function (e) {
+    removeIndex(oneName.id)
+    saveNames(names)
+    textFieldCreateAgain(names)
+  })
+
   newSpan.textContent = oneName.firstName
   newDiv.appendChild(newSpan)
 
   const textField = document.querySelector(".text-field")
   textField.appendChild(newDiv)
+}
+
+//Zjištění indexu pro odstranění hodnoty z local storage
+
+const removeIndex = function (myID) {
+  const id = names.findIndex(function (checkedName) {
+    return checkedName.id === myID
+  })
+  if (id > -1) {
+    names.splice(id, 1)
+  }
+}
+
+// opětovná smazání textu z pole a naplnění dle localStorage
+
+const textFieldCreateAgain = (oneName) => {
+  const textField = document.querySelector(".text-field")
+  textField.innerHTML = ""
+
+  const myNames = JSON.parse(localStorage.getItem("names"))
+  myNames.forEach(function (item) {
+    textFieldCreate(item)
+  })
 }
