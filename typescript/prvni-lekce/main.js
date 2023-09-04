4585054177fb3828858fc540bff4263715b78412
Jakub Dvořák <jakubdvorak91@seznam.cz>
2023-09-04 20:18:59 +0200

    tyoescript slozka

---
+++ typescript/prvni-lekce/main.js
@@ -0,0 +1,7 @@
+var windows = 4;
+var doors = 1;
+var color = "bílá";
+var height = "50 metrů";
+var isNew = true;
+var garage = true;
+console.log(windows);
---
+++ typescript/prvni-lekce/main.ts
@@ -0,0 +1,8 @@
+const windows: number = 4
+const doors: number = 1
+const color: string = "bílá"
+const height: string = "50 metrů"
+const isNew: boolean = true
+const garage: boolean = true
+
+console.log(windows)
